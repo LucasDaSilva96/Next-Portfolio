4efd35e06ed7ac539f8a1a4c408970b2725c1d58
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-07-22 10:13:09 +0200

    Added a new project to showcase && added target _blanc to demo url's

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -62,6 +62,7 @@ export default function Projects() {
                     translateZ={20}
                     as={Link}
                     href={project.demo}
+                    target='__blank'
                     className='px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold'
                   >
                     Demo
